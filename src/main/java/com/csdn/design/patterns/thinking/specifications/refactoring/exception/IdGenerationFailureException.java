3d70c799a2bf9c80adfb898b697f429b392cd3a5
xiewenfeng <xiewenfeng@megvii.com>
2022-02-15 16:03:19 +0800

    feat:规范与重构：异常处理

---
+++ src/main/java/com/csdn/design/patterns/thinking/specifications/refactoring/exception/IdGenerationFailureException.java
@@ -0,0 +1,12 @@
+package com.csdn.design.patterns.thinking.specifications.refactoring.exception;
+
+/**
+ * @Author: xiewenfeng
+ * @Date: 2022/2/15 14:23
+ */
+public class IdGenerationFailureException extends Exception {
+
+  public IdGenerationFailureException(String message) {
+    super(message);
+  }
+}
--- src/main/java/com/csdn/design/patterns/thinking/specifications/refactoring/v2/IdGenerator.java
+++ src/main/java/com/csdn/design/patterns/thinking/specifications/refactoring/v2/IdGenerator.java
@@ -1,10 +1,12 @@
 package com.csdn.design.patterns.thinking.specifications.refactoring.v2;
 
+import com.csdn.design.patterns.thinking.specifications.refactoring.exception.IdGenerationFailureException;
+
 /**
  * @Author: xiewenfeng
  * @Date: 2022/2/15 11:13
  */
 public interface IdGenerator {
 
-  String generate();
+  String generate() throws IdGenerationFailureException;
 }
